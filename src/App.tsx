f149f015451c9472078ff2990390ac6ca5713f84
Yuri Oliveira <you@example.com>
2020-12-25 17:33:24 -0300

    feat: progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, { TrackPlayerEvents, STATE_PLAYING } from 'react-native-track-player';
+import {useTrackPlayerProgress} from 'react-native-track-player/lib/hooks';
+import {useTrackPlayerEvents} from 'react-native-track-player/lib/hooks';
 import Icon from 'react-native-vector-icons/Feather';
+import Slider from '@react-native-community/slider';
 
 async function trackPlayerInit() {
   await TrackPlayer.setupPlayer();
+
+  TrackPlayer.updateOptions({
+    stopWithApp: true,
+    capabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+    ],
+  });
 
   await TrackPlayer.add({
     id: '1',
@@ -17,11 +30,15 @@ async function trackPlayerInit() {
     artwork: 'https://picsum.photos/100',
   });
   return true;
- };
+};
+
 
 const App: React.FC = () => { 
  const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
  const [isPlaying, setIsPlaying] = useState(false);
+ const [sliderValue, setSliderValue] = useState(0);
+ const [isSeeking, setIsSeeking] = useState(false);
+ const {position, duration} = useTrackPlayerProgress(250);
  
  useEffect(() => {
    async function startPlayer() {
@@ -31,6 +48,20 @@ const App: React.FC = () => {
    }
    startPlayer();
  }, []);
+
+ useEffect(() => {
+  if (!isSeeking && position && duration) {
+    setSliderValue(position / duration);
+  }
+}, [position, duration]);
+
+useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], (event: any) => {
+  if (event.state === STATE_PLAYING) {
+    setIsPlaying(true);
+  } else {
+    setIsPlaying(false);
+  }
+});
 
 function handlePlay() {
   if (!isPlaying) {
@@ -42,6 +73,16 @@ function handlePlay() {
   }
 };
 
+const slidingStarted = () => {
+  setIsSeeking(true);
+};
+
+const slidingCompleted = async (value: number) => {
+  await TrackPlayer.seekTo(value * duration);
+  setSliderValue(value);
+  setIsSeeking(false);
+};
+
 
 return (
     <View>
@@ -51,8 +92,18 @@ return (
         ) : (
           <Icon name="play-circle" size={60} color="#000"/>
         )}
-        
       </TouchableOpacity>
+
+      <Slider 
+         style={{width: 400, height: 40}}
+         minimumValue={0}
+         maximumValue={1}
+         value={sliderValue}
+         minimumTrackTintColor="#111000"
+         maximumTrackTintColor="#000000"
+         onSlidingStart={slidingStarted}
+         onSlidingComplete={slidingCompleted}
+      />
     </View>
   )
 };
